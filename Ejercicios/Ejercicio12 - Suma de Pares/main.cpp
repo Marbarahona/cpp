94c3ab5d66998a2e1b6fd1f10bb519854ef70dc0
Marbarahona <98569646+Marbarahona@users.noreply.github.com>
2022-02-15 23:33:50 -0600

    Add files via upload
    agregando ejercicio 12 y 13

---
+++ Ejercicios/Ejercicio12 - Suma de Pares/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int pares = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 10) {
+            pares = pares + i;
+
+        }
+        cout << i << " ";
+
+    }
+
+
+    cout << endl;
+    cout << endl;
+    cout << "Total pares: " << pares; 
+    
+	return 0;
+}
+
