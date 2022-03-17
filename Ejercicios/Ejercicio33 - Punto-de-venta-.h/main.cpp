57e156cfef9439c912ab20e644a69709f3515163
Marbarahona <98569646+Marbarahona@users.noreply.github.com>
2022-03-16 21:29:54 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio33 - Punto-de-venta-.h/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "todo.h"
+
+using namespace std;
+
+void menu();
+
+int main(int argc, char const *argv[]){
+    
+menu();
+
+
+    return 0;
+}
---
+++ Ejercicios/Ejercicio33 - Punto-de-venta-.h/todo.h
@@ -0,0 +1,5 @@
+void agregarproducto();
+void productos(int opcion);
+void imprimirfactura();
+void menu();
+
