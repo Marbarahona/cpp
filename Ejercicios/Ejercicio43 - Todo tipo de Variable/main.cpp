a57e55c4e316fba2a81717a5e2420f3bb97fab47
Marbarahona <98569646+Marbarahona@users.noreply.github.com>
2022-03-29 23:15:10 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio43 - Todo tipo de Variable/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorBoolean;}
+    valorBoolean = true;
+
+    int valorEntero = 15;
+
+    double valorDouble = 20.99;
+
+    string valorString = "Hola, como estan ";
+
+    char valorChar = 'B';
+
+    cout << "El valor Boolean es: " << valorBoolean << endl;
+    cout << "El valor entero es: " << valorEntero << endl;
+    cout << "El valor double es: " << valorDouble << endl;
+    cout << "El valor string es: " << valorString << endl;
+    cout << "El valor char es: " << valorChar << endl;
+
+
+    return 0;
+}
+
